f7e6212b7415b8d86b31a3f27d4e58936de0f441
Beaux <daymon.allen@gmail.com>
2018-01-28 12:30:12 -0600

    Modularization
    Modularize CSS, complete primary navigaton, structure base views

--- js/main.js
+++ js/main.js
@@ -4,7 +4,23 @@ function toggleNav() {
 		document.getElementById("side-menu").style.left = "0px";
 		navStatus = 1;
 	} else if (navStatus == 1) {
-		document.getElementById("side-menu").style.left = "-200px";
+		document.getElementById("side-menu").style.left = "-100vw";
 		navStatus = 0;
 	}
 }
+
+
+
+
+
+/*
+let sectionStatus = 0;
+function sectionIn(el, pos) {
+	if (sectionStatus == 0) {
+	var elem = document.getElementById(el)
+	elem.style.display = pos
+	sectionStatus = 1
+} else if (sectionStatus == 1) {
+	el.style.marginLeft = none
+}
+*/
